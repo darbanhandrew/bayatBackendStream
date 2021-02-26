08dce04ce4c57c13bd16a06934cd08f7dfd9a289
Mohammad <darbanhandrew@gmail.com>
2021-02-27 01:39:34 +0330

    removed bulk user add!

--- app/bayatStream/urls.py
+++ app/bayatStream/urls.py
@@ -21,11 +21,8 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
-from app.oneTime.views import userdata
-
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-                  path('userdata/', userdata, name='userdata')
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                            document_root=settings.MEDIA_ROOT)
